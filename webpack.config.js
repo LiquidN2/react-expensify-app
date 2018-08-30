443e427e6a43190be5a1aaf44d945b4c3dfcec22
Hugh Nguyen <hhnguyen255@gmail.com>
2018-08-30 15:47:44 +1000

    initial react router setup

--- webpack.config.js
+++ webpack.config.js
@@ -17,14 +17,19 @@ module.exports = {
             }
         }, {
             test: /\.s?css$/,
-            use: ['style-loader', 'css-loader', 'sass-loader']
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
         }]
     },
 
     devtool: 'cheap-module-eval-source-map',
 
     devServer: {
-        contentBase: path.resolve(__dirname, 'public')
+        contentBase: path.resolve(__dirname, 'public'),
+        historyApiFallback: true // this will inform the browser to use client-side routing instead of server-side routing
     },
     
     watch: true
