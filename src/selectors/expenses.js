547342b96da29eee07ed9f9b1623859c88ffdbcc
Hugh Nguyen <hhnguyen255@gmail.com>
2018-09-05 10:30:09 +1000

    add selectors/expenses.test.js

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -2,6 +2,7 @@ import moment from 'moment';
 
 // Get Visible Expenses
 const getVisibleExpenses = (expenses, {text, sortBy, startDate, endDate}) => {
+    // startDate & endDate properties of filter object are moment instances
     const results = expenses.filter(expense => {
         const createdMoment = moment(expense.createdAt);
         const startDateMatch = startDate ? startDate.isSameOrBefore(createdMoment, 'day') : true;
@@ -9,6 +10,7 @@ const getVisibleExpenses = (expenses, {text, sortBy, startDate, endDate}) => {
         const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
         return startDateMatch && endDateMatch && textMatch;
     }).sort((a, b) => {
+        // sort in descending order
         if(sortBy === 'date') {
             // return a.createdAt < b.createdAt ? 1 : -1;
             return b.createdAt - a.createdAt;
