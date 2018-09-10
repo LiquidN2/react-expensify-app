d54cb85d64161fdbd43b3951e092faffb19acd77
Hugh Nguyen <hhnguyen255@gmail.com>
2018-09-11 09:30:34 +1000

    setup numeraljs formating for amount

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import moment from 'moment';
+import numeral from 'numeral';
 
 // moment.locale('en-au');
-const ExpenseListItem = ({id, description, amount, createdAt}) => (
+const ExpenseListItem = ({ id, description, amount, createdAt }) => (
     <div>
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
         </Link>
-        <p>${parseFloat(amount/100).toFixed(2)} - {moment(createdAt).format('L')}</p>
+        <p>
+            {numeral(amount / 100).format('$0,0.00')}
+            -
+            {moment(createdAt).format('L')}
+        </p>
     </div>
 );
 
