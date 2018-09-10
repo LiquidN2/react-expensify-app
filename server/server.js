61e273e72a404a74bcf817a72e571691c5e968ad
Hugh Nguyen <hhnguyen255@gmail.com>
2018-09-10 18:18:54 +1000

    Setup production build and server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 const publicPath = path.resolve(__dirname, '..', 'public');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
